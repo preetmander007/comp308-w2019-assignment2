703e945b34d1074e8324785d184e480240ff1da9
preetmander007 <preetmander007@gmail.com>
2019-04-05 17:10:46 -0400

    second one

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 // Components
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
-import { ProductsComponent } from './pages/products/products.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -19,7 +19,7 @@ import { AuthGuard } from './guards/auth.guard';
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
   {path: 'about', component: AboutComponent, data: {title: 'About'}},
-  {path: 'products', component: ProductsComponent, data: {title: 'Products'}},
+  {path: 'projects', component: ProjectsComponent, data: {title: 'Projects'}},
   {path: 'services', component: ServicesComponent, data: {title: 'Services'}},
   {path: 'contact', component: ContactComponent, data: {title: 'Contact'}},
 
--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
    //"URI" : "mongodb://localhost/test",
-    "URI" : "mongodb://thomas:a123456@ds060977.mlab.com:60977/comp308-w2019-lesson10b",
+    "URI" : "mongodb://manpreet:a123456@ds035787.mlab.com:35787/assignment2-manpreet",
     "secret": "SomeSecret"
 }
